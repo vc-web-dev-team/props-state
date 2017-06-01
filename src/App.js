e8b1923f981b1279aed62dbb9178b2609d9b5154
Krystal Klumpp <kklumpp13@hotmail.com>
2017-06-01 17:51:37 +0000

    First commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Email from './components/Email' 
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Email />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Email.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Email extends Component {
+   render () {
+       return (
+        <h1>Testing!!</h1>
+       )
+   }
+};
+
+export default Email;
